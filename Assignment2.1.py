da9b0b50d84078798cecf3ca3d1138eab58f47c2
rohit391 <rh812457@dal.ca>
2018-10-04 22:28:05 -0300

    Attempt1

---
+++ Assignment2.1.py
@@ -0,0 +1,17 @@
+fin = open("Crime.csv")
+for line in fin:
+  word = line.split()
+  
+def crime():
+  print(" Crime Type | Crime ID | Crime Count")
+  
+  if (word == "ASSAULT" and word =="ROBBERY" and word == "THEFT OF VEHICLE" and word == "BREAK AND ENTER"):
+    print (word)
+    
+    
+lst = []
+for i in word:
+    lst.append(i)
+crime
+	
+
